dd5f19c5b2479674810bab6313d1ccf549436ccf
PAVITHRAVIMALA <41355937+PAVITHRAVIMALA@users.noreply.github.com>
2018-07-18 14:59:35 +0530

    Add files via upload

---
+++ matplotlib+code.py
@@ -0,0 +1,123 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+print("hello")
+
+
+# In[4]:
+
+
+a = 6
+b = 8
+c = a + b
+print(c)
+
+
+# In[5]:
+
+
+print("hi",c)
+
+
+# In[6]:
+
+
+a=5
+if(a==5):
+    print("abc")
+
+
+# In[9]:
+
+
+a=5
+if(a!=5):
+    print("abc")
+else:
+    print("xyz")
+
+
+# In[10]:
+
+
+a=15
+if(a==5):
+    print("abc")
+elif(a>10):
+    print("def")
+else:
+    print("xyz")
+
+
+# In[11]:
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.ylabel('numbers')
+plt.show()
+
+
+# In[12]:
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.title('graph')
+plt.ylabel('ages')
+plt.ylabel('disease rate')
+plt.show()
+
+
+# In[18]:
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.plot([1,2,3,4,5,6,71])
+plt.title('graph')
+plt.ylabel('ages')
+plt.ylabel('disease rate')
+plt.show()
+
+
+# In[23]:
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.plot([100,202,303,404,505,])
+plt.title('graph')
+plt.ylabel('Student')
+plt.ylabel('Height')
+plt.show()
+
+
+# In[27]:
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.plot([1,2,3,4,5,6,71])
+plt.title('graph')
+plt.ylabel('ages')
+plt.ylabel('disease rate')
+plt.show()
+plt.savefig('graph.png')
+
+
+# In[30]:
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.plot([1,2,3,4,5,6,71])
+plt.title('graph')
+plt.ylabel('ages')
+plt.ylabel('disease rate')
+plt.show()
+plt.savefig('graph.png')
+
